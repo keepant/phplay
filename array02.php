84a8b200e434a21494e9402c1ea946a18d230e47
Irfan Dwi Prasetyo <keepant@gmail.com>
2015-10-12 17:40:43 +0700

    deklarasi array

---
+++ array02.php
@@ -0,0 +1,9 @@
+<?php
+	$arwarna = array();
+	$arwarna[] = "Merah";
+	$arwarna[] = "Biru";
+	$arwarna[] = "Hijau";
+	$arwarna[] = "Putih";
+	echo "Array Warna: <br>";
+	echo "$arwarna[0]<br>";
+	echo "$arwarna[2]<br>";
