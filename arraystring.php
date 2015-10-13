89b66d3a4fa362777425017f080bc2b0fc43bd90
Irfan Dwi Prasetyo <keepant@gmail.com>
2015-10-13 17:44:17 +0700

    deklarasi array string

---
+++ arraystring.php
@@ -0,0 +1,11 @@
+<?php
+	$arnilai = array ("Ani" => 80, "Otim" =>90, "Ana" => 75, "Budi" => 85);
+	echo $arnilai["Ani"];
+	echo $arnilai['Otim'];
+	//atau
+	$arnilai = array();
+	$arnilai['Ami'] = 80;
+	$arnilai['Asma'] = 95;
+	$arnilai['Sri'] = 77;
+	echo $arnilai['Asma'];
+	echo $arnilai['Ami'];
